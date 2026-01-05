e49c073069a26b4f8b4476fd6678ee16d6985bc8
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-05 20:32:33 -0300

    refatora componente NavMain para aceitar título como prop

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -9,11 +9,16 @@ import { resolveUrl } from '@/lib/utils';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 
-export function NavMain({ items = [] }: { items: NavItem[] }) {
+type NavMainProps = {
+    title?: string;
+    items: NavItem[];
+};
+
+export function NavMain({ title, items }: NavMainProps) {
     const page = usePage();
     return (
         <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarGroupLabel>{title}</SidebarGroupLabel>
             <SidebarMenu>
                 {items.map((item) => (
                     <SidebarMenuItem key={item.title}>
