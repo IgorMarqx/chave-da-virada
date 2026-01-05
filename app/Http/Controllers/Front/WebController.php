e53e14e85d63759f6237f8dfabb13076bd19fbdd
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-05 20:32:55 -0300

    adiciona método dashboard ao WebController para renderizar a página inicial

--- app/Http/Controllers/Front/WebController.php
+++ app/Http/Controllers/Front/WebController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 
 class WebController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        return Inertia::render('dashboard');
+    }
 }
