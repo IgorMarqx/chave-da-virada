af6515110d01ac80b02c20d06cfa4970868f92c1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-06 13:26:30 -0300

    adiciona controlador ConcursosController

---
+++ app/Http/Controllers/Api/ConcursosController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Services\ConcursosService;
+use Illuminate\Http\Request;
+
+class ConcursosController extends ApiController
+{
+    public function __construct(private readonly ConcursosService $concursosService) {}
+
+    public function create(Request $request)
+    {
+        $data = $request->validate([
+            'nome' => 'required|string|max:255',
+            'orgao' => 'required|string|max:255',
+            'data_prova' => 'nullable|date',
+            'descricao' => 'nullable|string',
+        ]);
+
+        $concurso = $this->concursosService->create($data);
+
+        return $this->apiSuccess($concurso, 'Concurso created successfully', 201);
+    }
+
+    public function index()
+    {
+        $user = request()->user();
+        $concursos = $this->concursosService->listForUser($user->id);
+
+        return $this->apiSuccess($concursos, 'Concursos fetched successfully');
+    }
+}
