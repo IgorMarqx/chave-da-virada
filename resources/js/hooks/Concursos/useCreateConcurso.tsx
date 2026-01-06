c0237b6676526c69358c4f8a7469654ba3dab91a
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-06 13:27:06 -0300

    refatora hook useCreateConcurso para melhorar tratamento de erros e validação de dados

--- resources/js/hooks/Concursos/useCreateConcurso.tsx
+++ resources/js/hooks/Concursos/useCreateConcurso.tsx
@@ -1,25 +1,57 @@
 import { notifications } from "@/components/ui/notification";
-import { isApiError } from "@/lib/http";
+import { http, isApiError } from "@/lib/http";
+import { CreateConcursoData } from "@/types/Concursos";
 import { useState } from "react";
 
 export function useCreateConcurso() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
+    const [isSuccess, setIsSuccess] = useState(false);
 
-    try {
-        
-    } catch (err) {
-        if (isApiError(err)) {
-            if (err.response?.data?.message) {
-                setError(err.response.data.message);
-                notifications.danger(err.response.data.message);
-                return;
-            }
-        } else {
-            setError('An unexpected error occurred. Please try again.');
-            notifications.danger('An unexpected error occurred. Please try again.');
+    const handleCreateConcurso = async (data: CreateConcursoData) => {
+        setError(null);
+        if (!validateData(data)) {
+            return;
         }
-    } finally {
-        setIsLoading(false);
+
+        setIsLoading(true);
+        try {
+            const response = await http.post('/concursos/create', { ...data });
+
+            if (response.data.success) {
+                notifications.success('Concurso criado com sucesso!');
+            }
+        } catch (err) {
+            if (isApiError(err)) {
+                if (err.response?.data?.message) {
+                    setError(err.response.data.message);
+                    notifications.danger(err.response.data.message);
+                    return;
+                }
+            } else {
+                setError('An unexpected error occurred. Please try again.');
+                notifications.danger('An unexpected error occurred. Please try again.');
+            }
+        } finally {
+            setIsLoading(false);
+        }
     }
+
+    const validateData = (data: CreateConcursoData) => {
+        if (data.nome.trim() === '') {
+            notifications.danger('O nome do concurso é obrigatório.');
+            setError('O nome do concurso é obrigatório.');
+            return false;
+        }
+
+        if (data.orgao.trim() === '') {
+            notifications.danger('O órgão do concurso é obrigatório.');
+            setError('O órgão do concurso é obrigatório.');
+            return false;
+        }
+
+        return true;
+    }
+
+    return { isLoading, error, handleCreateConcurso };
 }
