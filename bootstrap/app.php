cc81e43b5ff2828ce4f7fa3f7c05fdb6cdbdde35
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-06 11:48:06 -0300

    adiciona alias para middleware de autenticação JWT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->alias([
+            'jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
