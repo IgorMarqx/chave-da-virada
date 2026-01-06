e304074f6c715002ae158e587c022a32968fc660
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-06 16:50:15 -0300

    adiciona métodos para exibir disciplinas e tópicos de concursos no controlador WebController

--- app/Http/Controllers/Front/WebController.php
+++ app/Http/Controllers/Front/WebController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Concurso;
+use App\Models\Disciplina;
+use App\Models\Topico;
 use Inertia\Inertia;
 
 class WebController extends Controller
@@ -15,5 +18,33 @@ class WebController extends Controller
     public function estudos()
     {
         return Inertia::render('Estudos/index');
+    }
+
+    public function concursoDisciplinas(Concurso $concurso)
+    {
+        return Inertia::render('Estudos/Concursos/index', [
+            'concurso' => $concurso->only('id', 'nome'),
+        ]);
+    }
+
+    public function disciplinaTopicos(Disciplina $disciplina)
+    {
+        return Inertia::render('Estudos/Disciplinas/index', [
+            'disciplina' => $disciplina->only('id', 'nome', 'concurso_id'),
+        ]);
+    }
+
+    public function topicoDetalhe(Topico $topico)
+    {
+        $topico->load('disciplina:id,concurso_id');
+
+        return Inertia::render('Estudos/Topicos/index', [
+            'topico' => [
+                'id' => $topico->id,
+                'nome' => $topico->nome,
+                'disciplina_id' => $topico->disciplina_id,
+                'concurso_id' => $topico->disciplina->concurso_id ?? null,
+            ],
+        ]);
     }
 }
