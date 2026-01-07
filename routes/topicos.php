bd0e05957b56f2c8a812737bfb411c7fc7579346
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-07 20:14:17 -0300

    adiciona rotas para atualização e remoção de tópicos

--- routes/topicos.php
+++ routes/topicos.php
@@ -4,5 +4,8 @@ use App\Http\Controllers\Api\TopicosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('disciplina/{disciplina}', [TopicosController::class, 'listByDisciplina']);
+Route::patch('ordem', [TopicosController::class, 'updateOrder']);
 Route::get('{topico}', [TopicosController::class, 'show']);
 Route::post('create', [TopicosController::class, 'create']);
+Route::patch('{topico}', [TopicosController::class, 'update']);
+Route::delete('{topico}', [TopicosController::class, 'destroy']);
