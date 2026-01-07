8836519e9d56f4f137e63e91db062a9cac07fa1c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-06 22:19:11 -0300

    adiciona rota para revisão de tópicos no WebController

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('estudos', [WebController::class, 'estudos'])->name('estudos');
 Route::get('estudos/concursos/{concurso}', [WebController::class, 'concursoDisciplinas'])->name('estudos.concursos');
 Route::get('estudos/disciplinas/{disciplina}', [WebController::class, 'disciplinaTopicos'])->name('estudos.disciplinas');
 Route::get('estudos/topicos/{topico}', [WebController::class, 'topicoDetalhe'])->name('estudos.topicos');
+Route::get('estudos/topicos/{topico}/revisao', [WebController::class, 'topicoRevisao'])->name('estudos.topicos.revisao');
 
 require __DIR__ . '/settings.php';
