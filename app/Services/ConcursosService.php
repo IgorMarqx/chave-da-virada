b6df42c9377ecd9baaa8734b4d24fb565ca3715f
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-07 20:12:02 -0300

    adiciona tipagem ao método listForUser no ConcursosService

--- app/Services/ConcursosService.php
+++ app/Services/ConcursosService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Concurso;
 use App\Repositories\ConcursosRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class ConcursosService
 {
@@ -16,8 +17,18 @@ class ConcursosService
         return $this->concursosRepository->create($data);
     }
 
-    public function listForUser(int $userId)
+    public function listForUser(int $userId): Collection
     {
         return $this->concursosRepository->allWithProgressForUser($userId);
+    }
+
+    public function update(Concurso $concurso, array $data): Concurso
+    {
+        return $this->concursosRepository->update($concurso, $data);
+    }
+
+    public function delete(Concurso $concurso): void
+    {
+        $this->concursosRepository->delete($concurso);
     }
 }
