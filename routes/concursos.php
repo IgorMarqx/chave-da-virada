f0c8af1e6ff841e56058543567732d36bef0073d
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-07 20:14:12 -0300

    adiciona rotas para atualização e remoção de concursos

--- routes/concursos.php
+++ routes/concursos.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ConcursosController::class, 'index']);
 Route::post('create', [ConcursosController::class, 'create']);
+Route::patch('{concurso}', [ConcursosController::class, 'update']);
+Route::delete('{concurso}', [ConcursosController::class, 'destroy']);
