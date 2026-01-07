99edb54dcd9250d66e693a0a038e1080dfedd0cb
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-07 20:12:04 -0300

    adiciona tipagem aos métodos listByConcursoForUser e listRecentForUser no DisciplinasService

--- app/Services/DisciplinasService.php
+++ app/Services/DisciplinasService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Disciplina;
 use App\Repositories\DisciplinasRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class DisciplinasService
 {
@@ -16,13 +17,23 @@ class DisciplinasService
         return $this->disciplinasRepository->create($data);
     }
 
-    public function listByConcursoForUser(int $concursoId, int $userId)
+    public function listByConcursoForUser(int $concursoId, int $userId): Collection
     {
         return $this->disciplinasRepository->listByConcursoForUser($concursoId, $userId);
     }
 
-    public function listRecentForUser(int $userId, int $limit = 3)
+    public function listRecentForUser(int $userId, int $limit = 3): Collection
     {
         return $this->disciplinasRepository->listRecentForUser($userId, $limit);
+    }
+
+    public function update(Disciplina $disciplina, array $data): Disciplina
+    {
+        return $this->disciplinasRepository->update($disciplina, $data);
+    }
+
+    public function delete(Disciplina $disciplina): void
+    {
+        $this->disciplinasRepository->delete($disciplina);
     }
 }
