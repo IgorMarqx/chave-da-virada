7e058c0c4de31a456a16526f6ab570ec9cd77b5a
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-07 15:07:00 -0300

    adiciona rotas para listar disciplinas recentes e registrar acesso

--- routes/disciplinas.php
+++ routes/disciplinas.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('create', [DisciplinasController::class, 'create']);
 Route::get('concurso/{concurso}', [DisciplinasController::class, 'listByConcurso']);
+Route::get('recent', [DisciplinasController::class, 'listRecent']);
+Route::post('{disciplina}/accessed', [DisciplinasController::class, 'markAccessed']);
