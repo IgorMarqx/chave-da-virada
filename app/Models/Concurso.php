7963be04c5c74d1c52b713507e8f61d1b8a31e1d
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-08 20:16:15 -0300

    adiciona relacionamento de disciplinas na classe Concurso

--- app/Models/Concurso.php
+++ app/Models/Concurso.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Concurso extends Model
 {
@@ -16,4 +17,9 @@ class Concurso extends Model
         'data_prova',
         'descricao',
     ];
+
+    public function disciplinas(): HasMany
+    {
+        return $this->hasMany(Disciplina::class);
+    }
 }
