e2e521e7469f2c474f52d30902d976034f52a171
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-08 20:20:54 -0300

    adiciona rota para listar tópicos no controlador de estudos

--- routes/estudos.php
+++ routes/estudos.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Api\EstudosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('topico/{topico}', [EstudosController::class, 'listByTopico']);
+Route::get('topicos', [EstudosController::class, 'listTopicos']);
 Route::post('/', [EstudosController::class, 'store']);
