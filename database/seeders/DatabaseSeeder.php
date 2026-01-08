d67c8ba70c7089024b642f2153897ee8b447a955
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-08 20:17:20 -0300

    adiciona chamadas para os seeders RevisaoConfiguracaoSeeder e RevisaoSemanalDemoSeeder no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
+        $this->call(RevisaoConfiguracaoSeeder::class);
+        $this->call(RevisaoSemanalDemoSeeder::class);
     }
 }
