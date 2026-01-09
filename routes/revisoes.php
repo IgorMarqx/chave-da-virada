7953e43462905c6cacb35b877e1537b9fd345bc1
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2026-01-09 16:34:49 -0300

    adiciona rota para iniciar revisões

--- routes/revisoes.php
+++ routes/revisoes.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\RevisoesController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('{id}/concluir', [RevisoesController::class, 'concluir']);
+Route::post('{id}/iniciar', [RevisoesController::class, 'iniciar']);
